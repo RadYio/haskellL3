45664e18bbb8c63010936621eeefbbe09fa34f59
Hadamar en Personne <allan.gonin-saget.etu@univ-lemans.fr>
2023-02-28 18:15:21 +0100

    ajout de la création d'un palindrome

--- TP1/palindrome.hs
+++ TP1/palindrome.hs
@@ -6,3 +6,7 @@ inverse (x:xs) = inverse xs ++ [x]
 --Fonction palindrome qui teste si une liste est un palindrome
 isPalindrome :: Eq a => [a] -> Bool
 isPalindrome xs = xs == inverse xs
+
+--Fonction doPalindrome qui créer un palindrome à partir d’une liste
+doPalindrome :: [a] -> [a]
+doPalindrome xs = xs ++ inverse (init xs)
